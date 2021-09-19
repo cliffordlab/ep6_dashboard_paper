a62026f4af6872d75599e1a864f89cc3af7f698a
Ratansingh648 <ratansingh648@gmail.com>
2021-09-18 21:00:46 -0400

    DAS-2 : Amending the environment config Logic

--- backend/run.py
+++ backend/run.py
@@ -7,27 +7,27 @@ import logging
 from config import config_dict
 from app import create_app, db
 
-# WARNING: Don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
-
-# The configuration
-get_config_mode = 'Debug' if DEBUG else 'Production'
+# Fetching the environment from local configuration file .env
+ENV = config('ENV', default="UAT")
 
 try:
-    
-    # Load the configuration using the default values 
-    app_config = config_dict[get_config_mode.capitalize()]
-
+    app_config = config_dict[ENV.upper()]
 except KeyError:
-    exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
+    exit('Error: Invalid configuration key "{}". Expected values [DEV, UAT, PROD]'.format(ENV.upper()))
+try:
+    DEBUG = app_config.DEBUG
+except:
+    DEBUG = False
 
-app = create_app( app_config ) 
+
+app = create_app(app_config)
 Migrate(app, db)
 
+
 if DEBUG:
-    app.logger.info('DEBUG       = ' + str(DEBUG)      )
-    app.logger.info('Environment = ' + get_config_mode )
-    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI )
+    app.logger.info('DEBUG       = ' + str(DEBUG))
+    app.logger.info('Environment = ' + ENV)
+    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI)
 
 if __name__ == "__main__":
     app.run()
