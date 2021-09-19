f025476a9ff729b6bc0cdd7fb884526672ea9e29
Ratansingh648 <ratansingh648@gmail.com>
2021-09-19 17:34:53 -0400

    DAS-3 : Connecting the Graph & Stats with backend

--- dashboard/src/components/stats/Stats.jsx
+++ dashboard/src/components/stats/Stats.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
 import './stats.css'
 
-export default function Stats() {
+export default function Stats(props) {
     return (
         <div className="stats">
             <div className="statsItem">
                 <span className="statsTitle">Mean</span>
                 <div className="statsContainer">
-                    <span className="statsValues">2.4 mV</span>
+                    <span className="statsValues">{props.stats.mean}</span>
                 </div>
             </div>
             <div className="statsItem">
                 <span className="statsTitle">Variance</span>
                 <div className="statsContainer">
-                    <span className="statsValues">2.4 mV</span>
+                    <span className="statsValues">{props.stats.variance}</span>
                 </div>
             </div>
             <div className="statsItem">
                 <span className="statsTitle">Median</span>
                 <div className="statsContainer">
-                    <span className="statsValues">2.4 mV</span>
+                    <span className="statsValues">{props.stats.median}</span>
                 </div>
             </div>
             <div className="statsItem">
                 <span className="statsTitle">Last 10</span>
                 <div className="statsContainer">
-                    <span className="statsValues">2.4 mV</span>
+                    <span className="statsValues">{props.stats.last10}</span>
                 </div>
             </div>
 
