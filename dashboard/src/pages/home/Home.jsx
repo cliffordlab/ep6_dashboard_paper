810c47c1434e800008b8b527a34bdc57502d1adc
Ratansingh648 <ratansingh648@gmail.com>
2021-09-27 02:49:12 -0400

    DAS-13 : Migrating the home to page

---
+++ dashboard/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Audio from '../audio/Audio'
+import Humidity from '../humidity/Humidity'
+import Visual from '../visual/Visual'
+
+
+import './home.css'
+
+
+export default function Home() {
+    return (
+        <div className="home">
+        </div>
+    )
+}
