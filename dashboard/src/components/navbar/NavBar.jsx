0f410d245835888058ef44d7a25a66920bcc5921
Arjun5inh <arjun.nakum@yahoo.com>
2021-09-28 00:24:39 -0400

    added a toggle button for light/dark mode

--- dashboard/src/components/navbar/NavBar.jsx
+++ dashboard/src/components/navbar/NavBar.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import * as React from 'react'
+import {Switch} from '@material-ui/core'
 import './navbar.css'
+
+const ToggleBtn=(e, val)=>{
+    console.log(val)
+}
 
 export default function NavBar() {
     return (
         <div className="navbar">
             <div className="navbarWrapper">
                 <span className="logo">EP6 Dashboard</span>
-                <div className="topRight">Right Data</div>
+                <Switch
+                color = "primary"
+                size = "medium"
+                onChange={ToggleBtn}
+                />
             </div>
         </div>
     )
