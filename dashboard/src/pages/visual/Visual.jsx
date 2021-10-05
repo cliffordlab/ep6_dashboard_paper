07f9abc996a594aece1bc613c189c39dd251757f
Ratansingh648 <ratansingh648@gmail.com>
2021-10-05 00:25:06 -0400

    DAS-20 : Adding Breadcrumbs

--- dashboard/src/pages/visual/Visual.jsx
+++ dashboard/src/pages/visual/Visual.jsx
@@ -1,4 +1,11 @@
 import React, {useState} from 'react'
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import HomeIcon from '@mui/icons-material/Home';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+
 import Stats from '../../components/stats/Stats'
 import './visual.css'
 
@@ -9,6 +16,13 @@ const Visual = (props) => {
 
    return (
       <div className="visual">
+
+         <Breadcrumbs aria-label="breadcrumb" sx={{ml : 3, mt: 3, mb : 1}}>
+            <Link underline="hover" sx={{ display: 'flex', alignItems: 'center' }} color="inherit" href="/"><HomeIcon sx={{ mr: 0.5 }} fontSize="13" font="roboto" />Home</Link>
+            <Link underline="hover" sx={{ display: 'flex', alignItems: 'center' }} color="inherit" href="/getting-started/installation/"> <DashboardIcon sx={{ mr: 0.5 }} fontSize="inherit" />Dashboard</Link>
+            <Typography sx={{ display: 'flex', alignItems: 'center' }} color="text.primary"> <CameraAltIcon sx={{ mr: 0.5 }} fontSize="inherit" /> Visuals </Typography>
+         </Breadcrumbs>
+
          <Stats stats={imageData}/>
          <div className="visualplot-wrapper">
             <img src='/visual/get-data' alt="home-map" useMap="#image-map">
