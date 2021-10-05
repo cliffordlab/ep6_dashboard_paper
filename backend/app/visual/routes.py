a3b78904246c058dd16f352cd8d1d5c82917317b
Ratansingh648 <ratansingh648@gmail.com>
2021-10-04 21:54:48 -0400

    DAS-20 : Removing hardcodings and adding API

--- backend/app/visual/routes.py
+++ backend/app/visual/routes.py
@@ -30,7 +30,62 @@ def visual_data():
     return send_file("visual//static//images//map.jpg", mimetype='image/jpg')
 
 
-@blueprint.route('/get-status')
+@blueprint.route('/get-layout')
+def get_layout():
+    return send_file("visual//static//images//layout.jpg", mimetype="image/jpg")
+
+
+@blueprint.route('/get-points')
+def get_points():
+    point_map = {'areas': [{
+        "name": "1",
+        "shape": "poly",
+        "coords": [25, 33, 27, 300, 128, 240, 128, 94],
+        "preFillColor": "transparent",
+        "fillColor": "#0000ff"
+    },
+        {
+        "name": "2",
+        "shape": "poly",
+        "coords": [219, 118, 220, 210, 283, 210, 284, 119],
+        "preFillColor": "transparent",
+        "lineWidth": 5,
+        "strokeColor": "#0000ff"
+    },
+        {
+        "name": "3",
+        "shape": "poly",
+        "coords": [381, 241, 383, 94, 462, 53, 457, 282],
+        "preFillColor": "transparent",
+        "lineWidth": 5,
+        "strokeColor": "#6afd09"
+    },
+        {
+        "name": "4",
+        "shape": "poly",
+        "coords": [245, 285, 290, 285, 274, 239, 249, 238],
+        "preFillColor": "transparent"
+    },
+        {
+        "name": "5",
+        "shape": "circle",
+        "coords": [170, 100, 25],
+        "preFillColor": "rgb(255,255,255,0.3)",
+        "lineWidth": 2
+    },
+        {
+        "name": "6",
+        "shape": "rect",
+        "coords": [270, 100, 200, 50],
+        "lineWidth": 2,
+        "preFillColor": "rgba(255, 255, 255, 0.3)",
+        "strokeColor": "#6afd09"
+    }
+    ]}
+    return point_map
+
+
+@ blueprint.route('/get-status')
 def get_status():
     rows = {"data": [{"name": "Kitchen PI - 04", "location": "Kitchen", "ipAddress": "192.168.0.13", "status": "connected"},
                      {"name": "Kitchen PI - 01", 'location': 'Kitchen', 'ipAddress': '192.168.0.7', 'status': 'disconnected'},
--- dashboard/src/components/layout/Layout.jsx
+++ dashboard/src/components/layout/Layout.jsx
@@ -1,61 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ImageMapper from '../imageMapper/ImageMapper'
 import './layout.css'
 
 const Layout = props => {
-  const URL = "https://c1.staticflickr.com/5/4052/4503898393_303cfbc9fd_b.jpg" ;
-  var MAP = {
-	name: "my-map",
-	areas: [
-		{
-			name: "1",
-			shape: "poly",
-			coords: [25, 33, 27, 300, 128, 240, 128, 94],
-			preFillColor: "transparent",
-			fillColor: "#0000ff"
-		},
-		{
-			name: "2",
-			shape: "poly",
-			coords: [219, 118, 220, 210, 283, 210, 284, 119],
-			preFillColor: "transparent",
-			lineWidth: 5,
-			strokeColor: "#0000ff"
-		},
-		{
-			name: "3",
-			shape: "poly",
-			coords: [381, 241, 383, 94, 462, 53, 457, 282],
-			preFillColor: "transparent", // this is mandatory for stroke color to work
-			lineWidth: 5,
-			strokeColor: "#6afd09"
-		},
-		{
-			name: "4",
-			shape: "poly",
-			coords: [245, 285, 290, 285, 274, 239, 249, 238],
-			preFillColor: "transparent"
-		},
-		{
-			name: "5",
-			shape: "circle",
-			coords: [170, 100, 25],
-			preFillColor: "rgb(255,255,255,0.3)",
-			lineWidth: 2
-		},
-		{
-			name: "6",
-			shape: "rect",
-			coords: [270, 100, 200, 50],
-			lineWidth: 2,
-			preFillColor: "rgba(255, 255, 255, 0.3)",
-			strokeColor: "#6afd09"
-		}
-	]
-};
+  var URL = "https://c1.staticflickr.com/4/4052/4503898393_303cfbc9fd_b.jpg" ;
+  const [layoutData, setLayoutData ] = useState();
+
+   useEffect(() => {
+      fetch('/visual/get-points').then(res => res.json()).then(data => {
+         setLayoutData({name: 'my-name', areas : data['areas']});
+      });
+   }, []);
+
   
   return <div className="layout">
-            <ImageMapper src={URL} map={MAP} width={500} />
+            <ImageMapper src={"/visual/get-layout"} map={layoutData} width={500} />
          </div>
 }
 
