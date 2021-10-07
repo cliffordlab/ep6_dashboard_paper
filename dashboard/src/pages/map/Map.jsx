7cc8847d7ced0341131b9316160d7648d8a029c0
Nick Shu <nicolas.s.shu@gmail.com>
2021-10-07 03:23:30 -0400

    Tried putting in d3

--- dashboard/src/pages/map/Map.jsx
+++ dashboard/src/pages/map/Map.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import './map.css'
+import {render} from 'react-dom'
+import { ReactSVG } from 'react';
+import * as d3 from "d3";
+
+import logo from "../../svgs/index.jpg";
+import img from "../../svgs/cam_regions_states.svg";
 
 
+console.log(d3.scaleTime())
 
 export default function Home() {
+    
     return (
         <div className="map">
-            <img src={"/svgs/ep6.svg"}></img>
+            <img src={img} />
+
+            {/* render( <ReactSVG src="../../svgs/cam_regions_states.svg" />, document.getElementById('root') ) */}
+            
+            
         </div>
     )
 }
