3fa53d5b7b440b0d2fba1e81cb9951721aab579d
Nick Shu <nicolas.s.shu@gmail.com>
2021-10-07 03:25:05 -0400

    Added mic img

--- dashboard/src/pages/map/Map.jsx
+++ dashboard/src/pages/map/Map.jsx
@@ -5,16 +5,18 @@ import { ReactSVG } from 'react';
 import * as d3 from "d3";
 
 import logo from "../../svgs/index.jpg";
-import img from "../../svgs/cam_regions_states.svg";
+import cam_img from "../../svgs/cam_regions_states.svg";
+import mic_img from "../../svgs/mic_regions_states.svg";
 
 
 console.log(d3.scaleTime())
 
 export default function Home() {
-    
+
     return (
         <div className="map">
-            <img src={img} />
+            <img src={cam_img} />
+            <img src={mic_img} />
 
             {/* render( <ReactSVG src="../../svgs/cam_regions_states.svg" />, document.getElementById('root') ) */}
             
