f6ed46f868f38602fec1b875c576eeaa5f11f97a
Ratansingh648 <ratansingh648@gmail.com>
2021-10-11 04:44:21 -0400

    DAS-20: Adding Click based plot

--- dashboard/src/pages/audio/Audio.jsx
+++ dashboard/src/pages/audio/Audio.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import { Line } from 'react-chartjs-2';
 
 import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
@@ -7,91 +6,50 @@ import Link from '@mui/material/Link';
 import HomeIcon from '@mui/icons-material/Home';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import MicIcon from '@mui/icons-material/Mic';
+import IconButton from '@mui/material/IconButton';
+import CancelIcon from '@mui/icons-material/Cancel';
+import Tooltip from '@mui/material/Tooltip';
 
 
 import Stats from '../../components/stats/Stats';
+import MicMap from '../../components/micMap/MicMap';
+import AudioPlot from '../../components/audioPlot/AudioPlot';
 
 import './audio.css'
 
 const Audio = (props) => {
 
    const [audioData, setAudioData] = useState({ data : { x : [], y: [] } , stat: { mean : 0, median : 0, variance : 0, last10 : 0 } });
+   const [showAudioGraph, setShowAudioGraph] = useState(false);
+
+   const regionClickHandler = (data) => {
+      setShowAudioGraph(data.showMap);
+   }
+
    useEffect(() => {
       fetch('/audio/get-data').then(res => res.json()).then(data => {
          setAudioData(data);
-      });
-   }, []);
+      }); }, []);
+   
 
    return (
       <div className="audio">
 
-         <Breadcrumbs aria-label="breadcrumb" sx={{ml : 3, mt: 3, mb : 1}}>
-            <Link underline="hover" sx={{ display: 'flex', alignItems: 'center' }} color="inherit" href="/"><HomeIcon sx={{ mr: 0.5 }} fontSize="13" font="roboto" />Home</Link>
-            <Link underline="hover" sx={{ display: 'flex', alignItems: 'center' }} color="inherit" href="/getting-started/installation/"> <DashboardIcon sx={{ mr: 0.5 }} fontSize="inherit" />Dashboard</Link>
-            <Typography sx={{ display: 'flex', alignItems: 'center' }} color="text.primary">
-            <MicIcon sx={{ mr: 0.5 }} fontSize="inherit" /> Audio </Typography>
-         </Breadcrumbs>
+            <Breadcrumbs aria-label="breadcrumb" sx={{ml : 3, mt: 3, mb : 1}}>
+               <Link underline="hover" sx={{ display: 'flex', alignItems: 'center' }} color="inherit" href="/"><HomeIcon sx={{ mr: 0.5 }} fontSize="13" font="roboto" />Home</Link>
+               <Link underline="hover" sx={{ display: 'flex', alignItems: 'center' }} color="inherit" href="/getting-started/installation/"> <DashboardIcon sx={{ mr: 0.5 }} fontSize="inherit" />Dashboard</Link>
+               <Typography sx={{ display: 'flex', alignItems: 'center' }} color="text.primary"> <MicIcon sx={{ mr: 0.5 }} fontSize="inherit" /> Audio </Typography>
+            </Breadcrumbs>
 
-         
-         <Stats stats={audioData.stat}/>
+            {showAudioGraph && <Tooltip title="Close Graph"><IconButton aria-label="Close Graph" className="close-button" onClick={() => setShowAudioGraph(false)}> <CancelIcon /> </IconButton></Tooltip> }  
+  
+
 
          <div className="audiochart-wrapper">
-            <Line
-               data = {{
-                  labels:  audioData.data.x,
-                  datasets: [
-                     {
-                        label: 'Audio 1',
-                        data :  audioData.data.y,
-                        borderColor: 'brown',
-                        borderWidth: 2,
-                        fill: true,
-                        lineTension: 0.5,
-                        pointRadius : 2,
-                        pointHoverRadius : 5,
-                        pointHoverBackgroundColor : 'rgb(127,127,127)'
-                     }
-                  ]
-               }}
-               height = {400}
-               width = {600}
-               options = {{
-                  responsive: true,
-                  title: {text: "iron man", display: true},
-                  maintainAspectRatio: false,
-                  scales: {
-                     yAxes: [
-                        {
-                           scaleLabel: {
-                              display: true,
-                              labelString: 'Y-Axis'
-                           },
-                           ticks: {
-                              autoSkip: true,
-                              maxTicksLimit: 10,
-                              beginAtZero: true
-                           }
-                        }
-                     ]
-                  },
-                  pan: {
-                     enabled: true,
-                     mode: "xy",
-                     speed: 10,
-                  },
-                  zoom: {
-                     wheel: {
-                        enabled: true,
-                     },
-                     drag: false,
-                     mode: "xy",
-                     limits: {
-                        x: {min: 0, max: 400, minRange: 10},
-                        y: {min: -200, max: 400, minRange: 10}
-                      },
-                  }
-               }}
-            /> 
+            { !showAudioGraph && <MicMap height={600} width={800} onclick={(e) => {regionClickHandler(e)}}/> }
+
+            { showAudioGraph && <Stats stats={audioData.stat}/> }
+            { showAudioGraph && <AudioPlot height={400} width={600} data={audioData.data} /> }
          </div>
       </div>
    )
