faf7a75cd12df85e5d1cb29e0fbbffcbd102f618
Ratansingh648 <ratansingh648@gmail.com>
2021-10-11 04:42:57 -0400

    DAS-20 : Making Audio Plot as component

---
+++ dashboard/src/components/audioPlot/AudioPlot.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+
+import { Line } from 'react-chartjs-2';
+
+
+export default function AudioPlot(props) {
+
+    return (
+        <div>
+            <Line
+               data = {{
+                  labels:  props.data.x,
+                  datasets: [
+                     {
+                        label: "Channel 1",
+                        data :  props.data.y[0],
+                        borderColor: '#7a5195',
+                        borderWidth: 2,
+                        fill: true,
+                        lineTension: 0.5,
+                        pointRadius : 2,
+                        pointHoverRadius : 5,
+                        pointHoverBackgroundColor : 'rgb(127,127,127)'
+                     },                     {
+                        label: "Channel 2",
+                        data :  props.data.y[1],
+                        borderColor: '#202020',
+                        borderWidth: 2,
+                        fill: true,
+                        lineTension: 0.5,
+                        pointRadius : 2,
+                        pointHoverRadius : 5,
+                        pointHoverBackgroundColor : 'rgb(127,127,127)',
+                        hidden: true
+                     }, 
+                    {
+                        label: "Channel 3",
+                        data :  props.data.y[2],
+                        borderColor: '#ef5675',
+                        borderWidth: 2,
+                        fill: true,
+                        lineTension: 0.5,
+                        pointRadius : 2,
+                        pointHoverRadius : 5,
+                        pointHoverBackgroundColor : 'rgb(127,127,127)',
+                        hidden: true
+                     },
+                     {
+                        label: "Channel 4",
+                        data :  props.data.y[3],
+                        borderColor: '#ffa600',
+                        borderWidth: 2,
+                        fill: true,
+                        lineTension: 0.5,
+                        pointRadius : 2,
+                        pointHoverRadius : 5,
+                        pointHoverBackgroundColor : 'rgb(127,127,127)',
+                        hidden: true
+                     }
+
+                  ]
+               }}
+               height = {props.height}
+               width = {props.width}
+               options = {{
+                  responsive: true,
+                  title: {text: "Audio", display: true},
+                  maintainAspectRatio: false,
+                  scales: {
+                     yAxes: [
+                        {
+                           scaleLabel: {
+                              display: true,
+                              labelString: 'dB'
+                           },
+                           ticks: {
+                              autoSkip: true,
+                              maxTicksLimit: 10,
+                              beginAtZero: true
+                           }
+                        }
+                     ]
+                  },
+                  pan: {
+                     enabled: true,
+                     mode: "xy",
+                     speed: 10,
+                  },
+                  zoom: {
+                     wheel: {
+                        enabled: true,
+                     },
+                     drag: false,
+                     mode: "xy",
+                     limits: {
+                        x: {min: 0, max: 400, minRange: 10},
+                        y: {min: -200, max: 400, minRange: 10}
+                      },
+                  }
+               }}
+            /> 
+        </div>
+    )
+}
