8340bbfeb0ba2e251c2c1246a25721010a8b6b87
Ratansingh648 <ratansingh648@gmail.com>
2021-10-16 03:16:39 -0400

    DAS-20: Removing Stats

--- dashboard/src/components/audioPlot/AudioPlot.jsx
+++ dashboard/src/components/audioPlot/AudioPlot.jsx
@@ -68,7 +68,6 @@ export default function AudioPlot(props) {
                height = {props.height}
                width = {props.width}
                options = {{
-
                   responsive: false,
                   title: {text: "Audio", display: true},
                   maintainAspectRatio: false,
--- dashboard/src/components/humidityPlot/HumidityPlot.jsx
+++ dashboard/src/components/humidityPlot/HumidityPlot.jsx
@@ -64,7 +64,8 @@ export default function HumidityPlot(props) {
                              },
                          },
                      },
-                  }}
+                  }]} 
+               }}
                /> 
       </div>
     )
