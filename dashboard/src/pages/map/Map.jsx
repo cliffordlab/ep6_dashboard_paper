e7d0c0e14a68f312d0be9944db9a5b50114756af
Ratansingh648 <ratansingh648@gmail.com>
2021-10-17 08:06:52 -0400

    DAS-20 : Removing redundancy

--- dashboard/src/pages/map/Map.jsx
+++ dashboard/src/pages/map/Map.jsx
@@ -1,26 +1,16 @@
 import React from 'react'
+import CameraMap from '../../components/cameraMap/CameraMap';
+import MicMap from '../../components/micMap/MicMap';
+
 import './map.css'
-import {render} from 'react-dom'
-import { ReactSVG } from 'react';
-import * as d3 from "d3";
-
-import logo from "../../svgs/index.jpg";
-import cam_img from "../../svgs/cam_regions_states.svg";
-import mic_img from "../../svgs/mic_regions_states.svg";
 
 
-console.log(d3.scaleTime())
+export default function Map() {
 
-export default function Home() {
 
     return (
         <div className="map">
-            <img src={cam_img} />
-            <img src={mic_img} />
-
-            {/* render( <ReactSVG src="../../svgs/cam_regions_states.svg" />, document.getElementById('root') ) */}
-            
-            
+            <CameraMap height={600} width={800}/>
         </div>
     )
 }
