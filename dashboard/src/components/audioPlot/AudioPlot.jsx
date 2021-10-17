c387f791e0ea75ea92ec4fc83c21e8ee4f7cd5b7
Ratansingh648 <ratansingh648@gmail.com>
2021-10-17 08:24:52 -0400

    DAS-20: Removing Stats

--- dashboard/src/components/audioPlot/AudioPlot.jsx
+++ dashboard/src/components/audioPlot/AudioPlot.jsx
@@ -68,7 +68,12 @@ export default function AudioPlot(props) {
                height = {props.height}
                width = {props.width}
                options = {{
+<<<<<<< HEAD
                   responsive: true,
+=======
+                  responsive: false,
+                  title: {text: "Audio", display: true},
+>>>>>>> 721605e... DAS-20: Removing Stats
                   maintainAspectRatio: false,
                   scales: {
                       yAxes: [
