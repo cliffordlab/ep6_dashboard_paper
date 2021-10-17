271b3b97af5e28accda857664ea1c09f155766c7
Ratansingh648 <ratansingh648@gmail.com>
2021-10-17 09:00:48 -0400

    DAS-20 : Changing order or braces

--- dashboard/src/components/humidityPlot/HumidityPlot.jsx
+++ dashboard/src/components/humidityPlot/HumidityPlot.jsx
@@ -63,6 +63,9 @@ export default function HumidityPlot(props) {
                                  speed: 100,
                              },
                          },
+                        }
+                       }
+                     ]
                      },
                   }}
                /> 
