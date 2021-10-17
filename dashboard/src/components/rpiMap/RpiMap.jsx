1ecb09024dbcb92c68cb138d5b94e61dd937cf0a
Ratansingh648 <ratansingh648@gmail.com>
2021-10-17 08:05:06 -0400

    DAS-20 :  Adding a map tab to RPi Tab

--- dashboard/src/components/rpiMap/RpiMap.jsx
+++ dashboard/src/components/rpiMap/RpiMap.jsx
@@ -2,10 +2,12 @@ import React, {useState, useEffect, useRef} from 'react'
 import {select, scaleLinear, scaleSequential} from 'd3';
 
 import swarm_data from './swarm_regions.json'; 
-import './micmap.css'
+import './rpimap.css'
+
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
 
 
-export default function MicMap(props) {
+export default function RpiMap(props) {
 
     const mapClickHandler = (region_id) => {
         console.log(region_id)     
@@ -19,9 +21,9 @@ export default function MicMap(props) {
     const ref = useRef()
 
     useEffect( () => {
-        select(".micMap").selectAll("*").remove()
+        select(".RpiMap").selectAll("*").remove()
 
-        const mic_svg = select(".micMap").append("svg").style("width", svg_w+"px").style("height", svg_h+"px");
+        const mic_svg = select(".RpiMap").append("svg").style("width", svg_w+"px").style("height", svg_h+"px");
 
         mics_map(swarm_data)
 
@@ -80,7 +82,7 @@ export default function MicMap(props) {
 
             
             // Plot the microphones
-            var mics = mic_svg.selectAll("circle")
+           var mics = mic_svg.selectAll("circle")
                         .data(loc_data)
                         .enter()
                         .append("circle")
@@ -99,33 +101,33 @@ export default function MicMap(props) {
                         })
 
 
-            // Put the texts
-            var nums = mic_svg.selectAll("text")
-                        .data(loc_data)
-                        .enter()
-                        .append("text")
-                        .attr("x", (d,i) => {return xscale(d.state[0])})
-                        .attr("y", (d,i) => {return yscale(d.state[1])})
-                        .text((d,i) => {return d.device})
-                        .attr('fill',"black")
-                        .attr("stroke","black")
-                        .attr("stroke-width", 2)
-                        .attr("id",(d,i) => {return "txt_"+d.device})
-                        .attr('font-size', 18)
-            
+             // Put the texts
+             var nums = mic_svg.selectAll("text")
+             .data(loc_data)
+             .enter()
+             .append("text")
+             .attr("x", (d,i) => {return xscale(d.state[0])})
+             .attr("y", (d,i) => {return yscale(d.state[1])})
+             .text((d,i) => {return d.device})
+             .attr('fill',"black")
+             .attr("stroke","black")
+             .attr("stroke-width", 2)
+             .attr("id",(d,i) => {return "txt_"+d.device})
+             .attr('font-size', 18)
+
             // Add Interactivity
             
             regs.on("mouseover", function(d){
                     select(this).attr("fill-opacity",1)
                     var dev = d.target.id.slice(4)
-                    select("#dev_"+dev).attr("r",10).attr("fill", "red")
+                    select("#dev_"+dev).attr("fill", "blue")
                     select("#reg_"+dev).attr("fill-opacity",1)
                     select("#txt_"+dev).attr("font-size",30).attr("fill","white")
                 })
                 .on("mouseout", function(d){
                     var dev = d.target.id.slice(4)
                     select(this).attr("fill-opacity",0.6)
-                    select("#dev_"+dev).attr("r",6).attr("fill","yellow")
+                    select("#dev_"+dev).attr("fill","green")
                     select("#reg_"+dev).attr("fill-opacity",0.6)
                     select("#txt_"+dev).attr("font-size",18).attr("fill","black")
                 })
@@ -145,7 +147,7 @@ export default function MicMap(props) {
 
 
     return (
-        <div className="micMap">
+        <div className="RpiMap">
         </div>
     )
 }
