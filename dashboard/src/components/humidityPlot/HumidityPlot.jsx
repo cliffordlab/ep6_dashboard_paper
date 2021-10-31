0c4641d89df26a9c223be79a4022ffeb2f9b90b6
Arjun5inh <arjun.nakum@yahoo.com>
2021-10-30 20:41:20 -0400

    Migrating code from page to component for audio and humidity

--- dashboard/src/components/humidityPlot/HumidityPlot.jsx
+++ dashboard/src/components/humidityPlot/HumidityPlot.jsx
@@ -7,68 +7,88 @@ Chart.register(zoomPlugin);
 export default function HumidityPlot(props) {
 
     return (
-      <div>
+      <div style={props.style}>
       <Line
-         data = {{
-            labels:  props.data.x,
-            datasets: [
-               {
-                  label: "Humidity",
-                  data :  props.data.humidity,
-                  borderColor: '#7a5195',
-                  borderWidth: 2,
-                  fill: true,
-                  lineTension: 0.5,
-                  pointRadius : 2,
-                  pointHoverRadius : 5,
-                  pointHoverBackgroundColor : 'rgb(127,127,127)'
-               },                     {
-                  label: "Temperature",
-                  data :  props.data.temperature,
-                  borderColor: '#202020',
-                  borderWidth: 2,
-                  fill: true,
-                  lineTension: 0.5,
-                  pointRadius : 2,
-                  pointHoverRadius : 5,
-                  pointHoverBackgroundColor : 'rgb(127,127,127)',
-                  hidden: true
-               }
-            ]
-         }}
-         height = {props.height}
-         width = {props.width}
-         options = {{
-            responsive: false,
-            title: {text: "Humidity", display: true},
-            maintainAspectRatio: false,
-            scales: {
-               yAxes: [
+          data={{
+              labels: props.data.x,
+              datasets: [
                   {
-                     scaleLabel: {
-                        display: true,
-                        labelString: 'dB'
-                     },
-                     plugins: {
-                         zoom: {
-                             pan: {
-                                 enabled: true,
-                                 mode: "xy",
-                             },
-                             zoom: {
-                                 wheel: {
-                                     enabled: true,
-                                 },
-                                 mode: "x",
-                                 speed: 100,
-                             },
-                         },
-                        }
-                       }
-                     ]
-                     },
-                  }} 
-               /> 
-      </div>
+                      label: "Temperature",
+                      data: props.data.humidity,
+                      borderColor: "brown",
+                      borderWidth: 2,
+                      fill: false,
+                      lineTension: 0.5,
+                      pointRadius: 1,
+                      pointHoverRadius: 5,
+                      pointHoverBackgroundColor: "rgb(127,127,127)",
+                  },
+                  {
+                      label: "Humidity",
+                      data: props.data.temperature,
+                      borderColor: "gold",
+                      borderWidth: 2,
+                      fill: false,
+                      lineTension: 0.5,
+                      pointRadius: 2,
+                      pointHoverRadius: 5,
+                      pointHoverBackgroundColor: "rgb(127,127,5)",
+                      hidden: true
+                  },
+              ],
+          }}
+          height={props.height}
+          width={props.width}
+          options={{
+              responsive: true,
+              maintainAspectRatio: false,
+              scales: {
+                  yAxes: {
+                      title: {
+                          padding: {
+                              top: 5,
+                          },
+                          display: true,
+                          text: "Y: Some Values",
+                          color: props.style.color,
+                          font: {
+                              size: 12,
+                              weight: 500,
+                          },
+                      },
+                  },
+                  xAxes: {
+                      title: {
+                          padding: {
+                              top: 5,
+                          },
+                          display: true,
+                          text: "X: Time Dependent",
+                          color: props.style.color,
+                          font: {
+                              size: 12,
+                              weight: 500,
+                          },
+                      },
+                  },
+              },
+              plugins: {
+                  zoom: {
+                      pan: {
+                          enabled: true,
+                          mode: "x",
+                      },
+                      zoom: {
+                          wheel: {
+                              enabled: true,
+                          },
+                          mode: "x",
+                          speed: 100,
+                      },
+                  },
+              },
+          }}
+      />
+  </div>
     )
 }
