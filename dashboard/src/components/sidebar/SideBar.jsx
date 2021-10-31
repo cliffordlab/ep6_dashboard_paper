157b45b67bf9123e31fb6d291c17f81a47492226
Arjun5inh <arjun.nakum@yahoo.com>
2021-10-30 20:40:28 -0400

    Adding components on Sidebar - Illuminance and Dashboard

--- dashboard/src/components/sidebar/SideBar.jsx
+++ dashboard/src/components/sidebar/SideBar.jsx
@@ -1,6 +1,6 @@
-import React, { useContext } from "react";
-import { NavLink, useLocation } from "react-router-dom";
-import clsx from "clsx";
+import React, {useContext} from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
+import clsx from 'clsx';
 
 import {
     GraphicEq,
@@ -15,12 +15,18 @@ import {
     PhotoCameraOutlined,
 } from "@material-ui/icons";
 
-import NetworkCheckOutlinedIcon from "@mui/icons-material/NetworkCheckOutlined";
-import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined";
-import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
+import MapIcon from '@mui/icons-material/Map';
+import HomeIcon from '@mui/icons-material/Home';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
+import KeyboardVoiceOutlinedIcon from '@mui/icons-material/KeyboardVoiceOutlined';
+import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
+import SettingsInputAntennaOutlinedIcon from '@mui/icons-material/SettingsInputAntennaOutlined';
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
-import { style, styled } from "@mui/system";
-import { useSwitch } from "@mui/core/SwitchUnstyled";
+import { styled } from '@mui/system';
+import { useSwitch } from '@mui/core/SwitchUnstyled';
 
 import "./sidebar.css";
 
@@ -85,89 +91,25 @@ export default function SideBar() {
                         {sideMenuShow && "Device Components"}
                     </h3>
                     <ul className="sidebarList">
-                        <NavLink to="/audio" className="link">
-                            <li
-                                className={
-                                    splitLocation[1] === "audio"
-                                        ? "sidebarListItem active"
-                                        : "sidebarListItem"
-                                }
-                            >
-                                <GraphicEqOutlined style={styles.sidebarIcon} />
-                                {sideMenuShow && "Audio"}
-                            </li>
-                        </NavLink>
-                        <NavLink to="/illuminance" className="link">
-                            <li
-                                className={
-                                    splitLocation[1] === "illuminance"
-                                        ? "sidebarListItem active"
-                                        : "sidebarListItem"
-                                }
-                            >
-                                <LightbulbOutlinedIcon
-                                    style={styles.sidebarIcon}
-                                />
-                                {sideMenuShow && "Illuminance"}
-                            </li>
-                        </NavLink>
-                        <NavLink to="/humidity" className="link">
-                            <li
-                                className={
-                                    splitLocation[1] === "humidity"
-                                        ? "sidebarListItem active"
-                                        : "sidebarListItem"
-                                }
-                            >
-                                <ThermostatAutoOutlined
-                                    style={styles.sidebarIcon}
-                                />
-                                {sideMenuShow && "Temperature"}
-                            </li>
-                        </NavLink>
-                        <NavLink to="/visual" className="link">
-                            <li
-                                className={
-                                    splitLocation[1] === "visual"
-                                        ? "sidebarListItem active"
-                                        : "sidebarListItem"
-                                }
-                            >
-                                <PhotoCameraOutlined
-                                    style={styles.sidebarIcon}
-                                />
-                                {sideMenuShow && "Visuals"}
-                            </li>
-                        </NavLink>
+                    <NavLink to="/" className="link"><li className={splitLocation[1] === "home" ? "sidebarListItem active" : "sidebarListItem"}><HomeIcon style={styles.sidebarIcon} />Home</li></NavLink>
+                    <NavLink to="/visual" className="link"><li className={splitLocation[1] === "visual" ? "sidebarListItem active" : "sidebarListItem"}><CameraAltOutlinedIcon style={styles.sidebarIcon} /> Visuals</li></NavLink>
+                    <NavLink to="/audio" className="link"><li className={splitLocation[1] === "audio" ? "sidebarListItem active" : "sidebarListItem"}><KeyboardVoiceOutlinedIcon style={styles.sidebarIcon} /> Audio</li></NavLink>
+                    <NavLink to="/illuminance" className="link"><li className={splitLocation[1] === "illuminance" ? "sidebarListItem active" : "sidebarListItem"}><LightbulbOutlinedIcon style={styles.sidebarIcon} /> Illuminance</li></NavLink>
+                    <NavLink to="/humidity" className="link"><li className={splitLocation[1] === "humidity" ? "sidebarListItem active" : "sidebarListItem"}><WbSunnyOutlinedIcon style={styles.sidebarIcon} /> Temperature</li></NavLink>
+                    <NavLink to="/map" className="link"><li className={splitLocation[1] === "map" ? "sidebarListItem active" : "sidebarListItem"}><MapIcon style={styles.sidebarIcon} />Map</li></NavLink>
                     </ul>
-                    <h3 style={styles.sidebarTitle}>
-                        <SettingsOutlined style={styles.sidebarIcon} />
-                        {sideMenuShow && "Settings"}
-                    </h3>
-                    <ul className="sidebarList">
-                        <div style={styles.otherIconsContainer}>
-                            <h3 style={styles.sidebarTitleOther}>
-                                <MUISwitch
-                                    color="primary"
-                                    size="small"
-                                    onChange={ToggleBtn}
-                                    defaultChecked
-                                    style={styles.sidebarIcon}
-                                />
-                                <span style={styles.others}>
-                                    {sideMenuShow && "Theme"}
-                                </span>
-                            </h3>
-                        </div>
-                        <h3 style={styles.sidebarTitleOther}>
-                            <PowerSettingsNewOutlined
-                                style={styles.sidebarIcon}
-                            />
-                            <span style={styles.team}>
-                                {sideMenuShow && "Arjun"}
-                            </span>
-                        </h3>
+
+                    <h3 style={styles.sidebarTitle}>Summarized View</h3>
+
+                    <ul style={styles.sidebarList}><NavLink to="/dashboard" className="link">
+                    <li className={splitLocation[1] === "dashboard" ? "sidebarListItem active" : "sidebarListItem"}><DashboardOutlinedIcon style={styles.sidebarIcon}/> Dashboard </li> </NavLink>
                     </ul>
+
+                </div>
+                <div style={styles.toggleTheme}>
+                    <div style={styles.darkMode}>
+                        <MUISwitch color="primary" size="small" onChange={ToggleBtn} defaultChecked/>
+                    </div>
                 </div>
             </div>
         </div>
@@ -271,7 +213,9 @@ const sideBarStyles = (mode) => ({
     },
 });
 
-const SwitchRoot = styled("span")(`
+
+
+const SwitchRoot = styled('span')(`
   display: inline-block;
   position: relative;
   width: 40px;
@@ -279,7 +223,7 @@ const SwitchRoot = styled("span")(`
   padding: 5px;
 `);
 
-const SwitchInput = styled("input")(`
+const SwitchInput = styled('input')(`
   position: absolute;
   width: 100%;
   height: 100%;
@@ -291,11 +235,11 @@ const SwitchInput = styled("input")(`
   cursor: pointer;
 `);
 
-const SwitchThumb = styled("span")(
-    ({ theme }) => `
+const SwitchThumb = styled('span')(
+  ({ theme }) => `
   position: absolute;
   display: block;
-  background-color: ${theme.palette.mode === "dark" ? "#003892" : "#001e3c"};
+  background-color: ${theme.palette.mode === 'dark' ? '#003892' : '#001e3c'};
   width: 30px;
   height: 30px;
   border-radius: 15px;
@@ -309,7 +253,7 @@ const SwitchThumb = styled("span")(
     width: 100%;
     height: 100%;
     background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-        "#fff"
+      '#fff',
     )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>') center center no-repeat;
   }
 
@@ -322,38 +266,38 @@ const SwitchThumb = styled("span")(
     
     &:before {
       background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-          "#fff"
+        '#fff',
       )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>');
     }
   }
-`
+`,
 );
 
-const SwitchTrack = styled("span")(
-    ({ theme }) => `
-  background-color: ${theme.palette.mode === "dark" ? "#8796A5" : "#aab4be"};
+const SwitchTrack = styled('span')(
+  ({ theme }) => `
+  background-color: ${theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be'};
   border-radius: 10px;
   width: 100%;
   height: 100%;
   display: block;
-`
+`,
 );
 
 function MUISwitch(props) {
-    const { getInputProps, checked, disabled, focusVisible } = useSwitch(props);
+  const { getInputProps, checked, disabled, focusVisible } = useSwitch(props);
 
-    const stateClasses = {
-        checked,
-        disabled,
-        focusVisible,
-    };
+  const stateClasses = {
+    checked,
+    disabled,
+    focusVisible,
+  };
 
-    return (
-        <SwitchRoot className={clsx(stateClasses)}>
-            <SwitchTrack>
-                <SwitchThumb className={clsx(stateClasses)} />
-            </SwitchTrack>
-            <SwitchInput {...getInputProps()} aria-label="Switch" />
-        </SwitchRoot>
-    );
+  return (
+    <SwitchRoot className={clsx(stateClasses)}>
+      <SwitchTrack>
+        <SwitchThumb className={clsx(stateClasses)} />
+      </SwitchTrack>
+      <SwitchInput {...getInputProps()} aria-label="Demo switch" />
+    </SwitchRoot>
+  );
 }
