86ee27a01d6706c76239fd3a44c5ce2b03e5f947
Ratansingh648 <ratansingh648@gmail.com>
2021-11-01 02:53:48 -0400

    DAS - 20 : Adding Configuration for Email Service

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -20,7 +20,7 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ('base', 'audio', 'visual', 'humidity', 'illuminance'):
+    for module_name in ('base', 'audio', 'visual', 'humidity', 'illuminance', 'mail'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
