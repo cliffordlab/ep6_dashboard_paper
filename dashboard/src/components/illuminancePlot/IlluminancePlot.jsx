d0759a53da2c1d583e1e5f874217ddb4abdfbd47
Arjun5inh <arjun.nakum@yahoo.com>
2021-11-03 00:10:22 -0400

    Added Illuminance page along with floorplan and theme

---
+++ dashboard/src/components/illuminancePlot/IlluminancePlot.jsx
@@ -0,0 +1,135 @@
+import React from 'react'
+
+import {Chart, Line } from 'react-chartjs-2';
+import zoomPlugin from "chartjs-plugin-zoom";
+
+
+Chart.register(zoomPlugin);
+
+
+export default function IlluminancePlot(props) {
+
+    console.log(props)
+
+    return (
+        <div style={props.style}>
+            <Line
+               data = {{
+                  labels:  props.data.x,
+                  datasets: [
+                     {
+                        label: "Channel 1",
+                        data :  props.data.Channel1,
+                        borderColor: 'maroon',
+                        borderWidth: 2,
+                        fill: false,
+                        lineTension: 0.5,
+                        pointRadius : 2,
+                        pointHoverRadius : 5,
+                        pointHoverBackgroundColor : 'rgb(127,127,127)',
+                     },                     {
+                        label: "Channel 2",
+                        data :  props.data.Channel2,
+                        borderColor: 'green',
+                        borderWidth: 2,
+                        fill: false,
+                        lineTension: 0.5,
+                        pointRadius : 2,
+                        pointHoverRadius : 5,
+                        pointHoverBackgroundColor : 'rgb(127,127,127)',
+                        hidden: true
+                     }, 
+                    {
+                        label: "Channel 3",
+                        data :  props.data.Channel3,
+                        borderColor: 'gold',
+                        borderWidth: 2,
+                        fill: false,
+                        lineTension: 0.5,
+                        pointRadius : 2,
+                        pointHoverRadius : 5,
+                        pointHoverBackgroundColor : 'rgb(127,127,127)',
+                        hidden: true
+                     },
+                     {
+                        label: "Channel 4",
+                        data :  props.data.Channel4,
+                        borderColor: 'blue',
+                        borderWidth: 2,
+                        fill: false,
+                        lineTension: 0.5,
+                        pointRadius : 2,
+                        pointHoverRadius : 5,
+                        pointHoverBackgroundColor : 'rgb(127,127,127)',
+                        hidden: true
+                     },
+                     {
+                        label: "Channel 5",
+                        data :  props.data.Channel5,
+                        borderColor: 'gray',
+                        borderWidth: 2,
+                        fill: false,
+                        lineTension: 0.5,
+                        pointRadius : 2,
+                        pointHoverRadius : 5,
+                        pointHoverBackgroundColor : 'rgb(127,127,127)',
+                        hidden: true
+                     }
+                  ]
+               }}
+               height = {350}
+               width = {600}
+               options = {{
+                  responsive: true,
+                  maintainAspectRatio: false,
+                  color: props.style.color,
+                  scales: {
+                    yAxes: {
+                        title: {
+                            padding: {
+                                top: 5,
+                            },
+                            display: true,
+                            text: " Illuminance (lm) ",
+                            color: props.style.color,
+                            font: {
+                                size: 12,
+                                weight: 500,
+                            },
+                        },
+                    },
+                    xAxes: {
+                        title: {
+                            padding: {
+                                top: 5,
+                            },
+                            display: true,
+                            text: "Time (sec)",
+                            color: props.style.color,
+                            font: {
+                                size: 12,
+                                weight: 500,
+                            },
+                        },
+                    },
+                  },
+                  plugins: {
+                      zoom: {
+                          pan: {
+                              enabled: true,
+                              mode: "x",
+                          },
+                          zoom: {
+                              wheel: {
+                                  enabled: true,
+                              },
+                              mode: "x",
+                              speed: 50,
+                          },
+                      },
+                  },
+               }}
+            /> 
+        </div>
+    )
+}
--- dashboard/src/pages/illuminance/Illuminance.jsx
+++ dashboard/src/pages/illuminance/Illuminance.jsx
@@ -1,164 +1,78 @@
-import React, { useEffect, useState } from "react";
-import { Chart, Line } from "react-chartjs-2";
-import zoomPlugin from "chartjs-plugin-zoom";
+import React, {useEffect, useState} from 'react';
+
+import IconButton from '@mui/material/IconButton'; 
+import CancelIcon from '@mui/icons-material/Cancel';
+import Tooltip from '@mui/material/Tooltip';
+
+import MicMap from '../../components/micMap/MicMap';
+import IlluminancePlot from '../../components/illuminancePlot/IlluminancePlot'; 
+
+import './illuminance.css'
 
 import { theme } from "../../theme/Themes";
 import { ThemeContext } from "../../theme/ThemeProvider";
 
-Chart.register(zoomPlugin);
-
 const Illuminance = (props) => {
-    const [illuminanceData, setIlluminanceData] = useState({
-        data: { x: [], Channel1: [], Channel2: [], Channel3: [], Channel4: [] },
-    });
-    useEffect(() => {
-        fetch("/illuminance/get-data")
-            .then((res) => res.json())
-            .then((data) => {
-                setIlluminanceData(data);
-                console.log("data fetched", data);
-            });
-    }, []);
 
-    const Chart = () => {
-        const [chartData, SetChartData] = useState({});
-    };
+   const [illuminanceData, setIlluminanceData] = useState({ data: { x: [], Channel1: [], Channel2: [], Channel3: [], Channel4: [], Channel5: [] }});
+   const [showIlluminanceGraph, setShowIlluminanceGraph] = useState(false);
 
-    const { mode } = React.useContext(ThemeContext);
-    const styles = illuminanceStyles(mode);
+   const regionClickHandler = (data) => {
+      setShowIlluminanceGraph(data.showMap);
+   }
 
-    return (
-        <div style={styles.illuminance}>
+   useEffect(() => {
+      fetch('/illuminance/get-data').then(res => res.json()).then(data => {
+         setIlluminanceData(data);
+      }); }, []);
+
+ 
+   const { mode } = React.useContext(ThemeContext);
+   const styles = illuminanceStyles(mode);
+   
+
+   return (
+      <div style={styles.illuminance}>
+
+{/*}
+            <Breadcrumbs aria-label="breadcrumb" sx={{ml : 3, mt: 3, mb : 1}}>
+               <Link underline="hover" sx={{ display: 'flex', alignItems: 'center' }} color={styles.illuminance.color} href="/"><HomeIcon sx={{ mr: 0.5 }} fontSize="13" font="roboto" />Home</Link>
+               <Link underline="hover" sx={{ display: 'flex', alignItems: 'center' }} color={styles.illuminance.color} href="/getting-started/installation/"> <DashboardIcon sx={{ mr: 0.5 }} fontSize="inherit" />Dashboard</Link>
+               <Typography sx={{ display: 'flex', alignItems: 'center' }} color={styles.illuminance.color}> <MicIcon sx={{ mr: 0.5 }} fontSize="inherit" /> Illuminance </Typography>
+            </Breadcrumbs>
+*/}
+
+            {showIlluminanceGraph && <Tooltip title="Close Graph"><IconButton aria-label="Close Graph" className="close-button" onClick={() => setShowIlluminanceGraph(false)}> <CancelIcon style={{fill: styles.illuminance.color}} /> </IconButton></Tooltip> }  
+  
+
+
+         <div style={styles.illuminancechartWrapper}>
+            { !showIlluminanceGraph && <MicMap height={350} width={450} onclick={(e) => {regionClickHandler(e)}}/> }
             
-            <div style={styles.illuminancechartWrapper}>
-                <Line
-                    data={{
-                        labels: illuminanceData.data.x,
-                        datasets: [
-                            {
-                                label: "Channel 1",
-                                data: illuminanceData.data.Channel1,
-                                borderColor: "maroon",
-                                borderWidth: 2,
-                                fill: false,
-                                lineTension: 0.5,
-                                pointRadius: 2,
-                                pointHoverRadius: 5,
-                                pointHoverBackgroundColor: "rgb(127,127,127)",
-                            },
-                            {
-                                label: "Channel 2",
-                                data: illuminanceData.data.Channel2,
-                                borderColor: "green",
-                                borderWidth: 2,
-                                fill: false,
-                                lineTension: 0.5,
-                                pointRadius: 2,
-                                pointHoverRadius: 5,
-                                pointHoverBackgroundColor: "rgb(127,127,127)",
-                            },
-                            {
-                                label: "Channel 3",
-                                data: illuminanceData.data.Channel3,
-                                borderColor: "gold",
-                                borderWidth: 2,
-                                fill: false,
-                                lineTension: 0.5,
-                                pointRadius: 2,
-                                pointHoverRadius: 5,
-                                pointHoverBackgroundColor: "rgb(127,127,127)",
-                            },
-                            {
-                                label: "Channel 4",
-                                data: illuminanceData.data.Channel4,
-                                borderColor: "blue",
-                                borderWidth: 2,
-                                fill: false,
-                                lineTension: 0.5,
-                                pointRadius: 2,
-                                pointHoverRadius: 5,
-                                pointHoverBackgroundColor: "rgb(127,127,127)",
-                            },
-                            {
-                                label: "Channel 5",
-                                data: illuminanceData.data.Channel5,
-                                borderColor: "gray",
-                                borderWidth: 2,
-                                fill: false,
-                                lineTension: 0.5,
-                                pointRadius: 2,
-                                pointHoverRadius: 5,
-                                pointHoverBackgroundColor: "rgb(127,127,127)",
-                            },
-                        ],
-                    }}
-                    height={400}
-                    width={600}
-                    options={{
-                        responsive: true,
-                        maintainAspectRatio: false,
-                        scales: {
-                            yAxes: {
-                                title: {
-                                    padding: {
-                                        top: 5,
-                                    },
-                                    display: true,
-                                    text: "Y: Some Values",
-                                    color: "gray",
-                                    font: {
-                                        size: 12,
-                                        weight: 500,
-                                    },
-                                },
-                            },
-                            xAxes: {
-                                title: {
-                                    padding: {
-                                        top: 5,
-                                    },
-                                    display: true,
-                                    text: "X: Time Dependent",
-                                    color: "gray",
-                                    font: {
-                                        size: 12,
-                                        weight: 500,
-                                    },
-                                },
-                            },
-                        },
-                        plugins: {
-                            zoom: {
-                                pan: {
-                                    enabled: true,
-                                    mode: "x",
-                                },
-                                zoom: {
-                                    wheel: {
-                                        enabled: true,
-                                    },
-                                    mode: "x",
-                                    speed: 100,
-                                },
-                            },
-                        },
-                    }}
-                />
-            </div>
-        </div>
-    );
-};
+            { showIlluminanceGraph && <IlluminancePlot height={350} width={600} data={illuminanceData.data} style={styles.illuminancePlot}/> }
+         </div>
+      </div>
+   )
+}
 
-export default Illuminance;
+export default Illuminance
+
 
 const illuminanceStyles = (mode) => ({
-    illuminance: {
-        flex: 4,
-        backgroundColor: theme[mode].backgroundColor,
-    },
-    illuminancechartWrapper: {
-        height: "60vh",
-        width: "96%",
-        marginRight: "15px",
-    },
+   illuminance: {
+       flex: 4,
+       backgroundColor: theme[mode].backgroundColor,
+       color: theme[mode].color
+   },
+
+   illuminancechartWrapper: {
+       height: "50vh",
+       width: "95%",
+       marginRight: "15px",
+   },
+
+   illuminancePlot: {
+      color: theme[mode].color
+   }
+
 });
