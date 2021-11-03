5497a4aa59d88fad25c60283fb1e8ac945dc6064
Arjun5inh <arjun.nakum@yahoo.com>
2021-11-03 00:15:19 -0400

    Completely re-did page theaming cus of merge error

--- dashboard/src/pages/visual/Visual.jsx
+++ dashboard/src/pages/visual/Visual.jsx
@@ -9,6 +9,8 @@ import IconButton from '@mui/material/IconButton';
 import CancelIcon from '@mui/icons-material/Cancel';
 import Tooltip from '@mui/material/Tooltip';
 
+import { theme } from "../../theme/Themes";
+import { ThemeContext } from "../../theme/ThemeProvider";
 
 import Stats from '../../components/stats/Stats'
 import CameraMap from '../../components/cameraMap/CameraMap';
@@ -24,19 +26,24 @@ const Visual = (props) => {
       setShowPosnet(data.showMap);
    }
 
-   return (
-      <div className="visual">
+   const { mode } = React.useContext(ThemeContext);
+   const styles = visualStyles(mode);
 
+   return (
+      <div style={styles.visual}>
+
+{/*
          <Breadcrumbs aria-label="breadcrumb" sx={{ml : 3, mt: 3, mb : 1}}>
             <Link underline="hover" sx={{ display: 'flex', alignItems: 'center' }} color="inherit" href="/"><HomeIcon sx={{ mr: 0.5 }} fontSize="13" font="roboto" />Home</Link>
             <Link underline="hover" sx={{ display: 'flex', alignItems: 'center' }} color="inherit" href="/getting-started/installation/"> <DashboardIcon sx={{ mr: 0.5 }} fontSize="inherit" />Dashboard</Link>
             <Typography sx={{ display: 'flex', alignItems: 'center' }} color="text.primary"> <CameraAltIcon sx={{ mr: 0.5 }} fontSize="inherit" /> Visuals </Typography>
          </Breadcrumbs>
+*/}
 
          {showPosnet && <Tooltip title="Close Graph"><IconButton aria-label="Close Image" className="close-button" onClick={() => setShowPosnet(false)}> <CancelIcon /> </IconButton></Tooltip> }  
          
-         <div className="visualplot-wrapper">
-            { !showPosnet && <CameraMap height={550} width={800}  onclick={(e) => {regionClickHandler(e)}} /> }
+         <div style={styles.visualchartWrapper}>
+            { !showPosnet && <CameraMap height={350} width={450}  onclick={(e) => {regionClickHandler(e)}} /> }
             { showPosnet && <img src="/visual/get-data" style={{"margin-top":"30px"}} />}
             
          </div>
@@ -45,3 +52,22 @@ const Visual = (props) => {
 }
 
 export default Visual
+
+const visualStyles = (mode) => ({
+   visual: {
+       flex: 4,
+       backgroundColor: theme[mode].backgroundColor,
+       color: theme[mode].color
+   },
+
+   visualchartWrapper: {
+       height: "50vh",
+       width: "95%",
+       marginRight: "15px",
+   },
+
+   visualPlot: {
+      color: theme[mode].color
+   }
+
+});
