60f6de667620cc40e8f1d4da6456b0a01c76159f
Arjun5inh <arjun.nakum@yahoo.com>
2021-11-03 01:35:17 -0400

    Even map can switch themes!

--- dashboard/src/components/rpiMap/RpiMap.jsx
+++ dashboard/src/components/rpiMap/RpiMap.jsx
@@ -4,6 +4,9 @@ import {select, scaleLinear, scaleSequential} from 'd3';
 import swarm_data from './swarm_regions.json'; 
 import './rpimap.css'
 
+import { theme } from "../../theme/Themes";
+import { ThemeContext } from "../../theme/ThemeProvider";
+
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
 
 
@@ -13,7 +16,10 @@ export default function RpiMap(props) {
         console.log(region_id)     
         props.onclick({region_id : region_id, showMap : true});
     }
- 
+
+    const { mode } = React.useContext(ThemeContext);
+    const styles = rpimapStyles(mode);
+
     const svg_h = props.height
     const svg_w = props.width
 
@@ -44,7 +50,6 @@ export default function RpiMap(props) {
 
             }
 
-
             // TODO Implement D3 vis here
             var xscale = scaleLinear().domain([-35,35]).range([0,svg_w]);
             var yscale = scaleLinear().domain([-15,25]).range([svg_h,0]);
@@ -73,9 +78,9 @@ export default function RpiMap(props) {
                         .attr('d',(d,i) => {
                             return d.path})
                         .style("fill",(d,i) => {return cscale(i)})
-                        .attr("stroke","white")
+                        .attr("stroke", theme[mode].color)
                         .attr("stroke-width",2)
-                        .attr("fill-opacity",0.6)
+                        .attr("fill-opacity",0.5)
                         .attr("id",(d,i) => {return "reg_"+d.device})
 
 
@@ -87,10 +92,10 @@ export default function RpiMap(props) {
                         .append("circle")
                         .attr("cx", (d,i) => {return xscale(d.state[0])})
                         .attr("cy", (d,i) => {return yscale(d.state[1])})
-                        .attr("r", 6)
+                        .attr("r", 5)
                         .attr('fill',"yellow")
-                        .attr("stroke","black")
-                        .attr("stroke-width", 3)
+                        .attr("stroke", theme[mode].color)
+                        .attr("stroke-width", 1)
                         .attr("id",(d,i) => {return "dev_"+d.device})
                         .on("mouseover", function(d) {
                             select(this).attr("r",10)
@@ -108,9 +113,9 @@ export default function RpiMap(props) {
              .attr("x", (d,i) => {return xscale(d.state[0])})
              .attr("y", (d,i) => {return yscale(d.state[1])})
              .text((d,i) => {return d.device})
-             .attr('fill',"black")
-             .attr("stroke","black")
-             .attr("stroke-width", 2)
+             .attr('fill', theme[mode].color)
+             .attr("stroke", theme[mode].color)
+             .attr("stroke-width", 0.5)
              .attr("id",(d,i) => {return "txt_"+d.device})
              .attr('font-size', 18)
 
@@ -150,3 +155,17 @@ export default function RpiMap(props) {
         </div>
     )
 }
+
+const rpimapStyles = (mode) => ({
+    rpimap: {
+        flex: 4,
+        backgroundColor: theme[mode].backgroundColor,
+        color: theme[mode].color
+    },
+ 
+    rpimapchartWrapper: {
+        height: "50vh",
+        width: "95%",
+        marginRight: "15px",
+    },
+ });
