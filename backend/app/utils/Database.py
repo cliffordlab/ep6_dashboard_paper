971943b1034aa252e5516312bcf51b7060ebc4a8
Ratansingh648 <ratansingh648@gmail.com>
2021-11-15 03:41:49 -0500

    DAS - 15 : Adding Logs to the files

--- backend/app/utils/Database.py
+++ backend/app/utils/Database.py
@@ -8,6 +8,7 @@
 
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
+import logging
 
 import socket
 from datetime import datetime
@@ -23,7 +24,7 @@ class Database():
         try:
             self.__client = InfluxDBClient(url=self.__url, token=self.__token, verify_ssl=verify_ssl)
         except Exception as e:
-            print(str(e))
+            logging.error("Exception occured in initializing the instance of DB", exc_info=True)
             print("Failed creating Influx DB Client.")
 
         self.__query_api = self.__client.query_api()
@@ -32,33 +33,36 @@ class Database():
         try:
             result = self.__query_api.query(query, org=self.__org)
         except Exception as e:
-            print(str(e))
-            print("An Exception occured while querying the database")
+            logging.error("Exception occured querying the database", exc_info=True)
             result = ""
         return result
 
     def __create_query(self, location, bucket, start, measurement, fields):
-        query_template = """
-        from(bucket:"{}")
-            |> range(start:{})
-            |> filter(fn: (r) => r["location"] == "{}")
-        """.format(bucket, start, location)
+        try:
+            query_template = """
+            from(bucket:"{}")
+                |> range(start:{})
+                |> filter(fn: (r) => r["location"] == "{}")
+            """.format(bucket, start, location)
 
-        query_template += '\n |> filter(fn: (r) => r["_measurement"] == "{}"'.format(measurement[0])
-        if len(measurement) > 1:
-            for measure in measurement[1:]:
-                query_template += ' or r["_measurement"] == "{}"'.format(measure)
-        query_template += ")"
+            query_template += '\n |> filter(fn: (r) => r["_measurement"] == "{}"'.format(measurement[0])
+            if len(measurement) > 1:
+                for measure in measurement[1:]:
+                    query_template += ' or r["_measurement"] == "{}"'.format(measure)
+            query_template += ")"
 
-        # Looping the fields
-        query_template += '\n |> filter(fn: (r) => r["_field"] == "{}"'.format(fields[0])
-        if len(fields) > 1:
-            for field in fields[1:]:
-                query_template += ' or r["_field"] == "{}"'.format(field)
-        query_template += ")"
+            # Looping the fields
+            query_template += '\n |> filter(fn: (r) => r["_field"] == "{}"'.format(fields[0])
+            if len(fields) > 1:
+                for field in fields[1:]:
+                    query_template += ' or r["_field"] == "{}"'.format(field)
+            query_template += ")"
 
-        print(query_template)
-        return query_template
+            print(query_template)
+            return query_template
+        except Exception as e:
+            logging.error("Exception occured in creating a query", exc_info=True)
+            return ""
 
     def __process_audio_data(self, data):
         result = []
@@ -67,7 +71,7 @@ class Database():
                 for record in table.records:
                     result.append({"channel": index, "time": record["_time"].strftime("%H:%M:%S"), "value": record["_value"]})
         except Exception as e:
-            print(str(e))
+            logging.error("Exception Occured in processing the audio data", exc_info=True)
         return result
 
     def __process_humidity_data(self, data):
@@ -77,17 +81,23 @@ class Database():
                 for record in table.records:
                     result.append({"measurement": record["_measurement"], "time": record["_time"].strftime("%H:%M:%S"), "value": record["_value"]})
         except Exception as e:
-            print(str(e))
+            logging.error("Exception Occured in processing the humidity data", exc_info=True)
         return result
 
     def query_audio(self, location, bucket="audio_features", start="-2m", measurement=["dBA"], fields=["1", "2", "3", "4"]):
-        query = self.__create_query(location, bucket, start, measurement, fields)
-        response = self.__query(query)
-        result = self.__process_audio_data(response)
-        return result
+        try:
+            query = self.__create_query(location, bucket, start, measurement, fields)
+            response = self.__query(query)
+            result = self.__process_audio_data(response)
+            return result
+        except Exception as e:
+            logging.error("Exception Occured querying the audio data", exc_info=True)
 
     def query_humidity(self, location, bucket="temp_hum_light", start="-2m", measurement=["humidity", "temperature"], fields=["celcius", "rel_humidity"]):
-        query = self.__create_query(location, bucket, start, measurement, fields)
-        response = self.__query(query)
-        result = self.__process_humidity_data(response)
-        return result
+        try:
+            query = self.__create_query(location, bucket, start, measurement, fields)
+            response = self.__query(query)
+            result = self.__process_humidity_data(response)
+            return result
+        except Exception as e:
+            logging.error("Exception Occured humidity data", exc_info=True)
--- backend/app/visual/routes.py
+++ backend/app/visual/routes.py
@@ -6,17 +6,14 @@
   Description : This is the file defining the routes for audio services.
 """
 
-from flask import jsonify, render_template, redirect, request, url_for, send_file
+from flask import jsonify, render_template, redirect, request, url_for, send_file, current_app
 import numpy as np
 import os
+import logging
+import logging.config
 
 from app import db
 from app.visual import blueprint
-
-
-@blueprint.route('/')
-def route_default():
-    return "Hello Lord Visual !"
 
 
 """
@@ -27,79 +24,95 @@ This data will be consumed by chart component and stats components
 
 @blueprint.route('/get-data')
 def visual_data():
-    print("Roger that")
-    path = os.path.join("visual","static","images","map.JPG")
-    return send_file(path, mimetype='image/jpg')
+    try:
+        path = os.path.join("visual", "static", "images", "map.JPG")
+        return send_file(path, mimetype='image/jpg')
+    except Exception as e:
+        current_app.logger.error("Exception Occured", exc_info=True)
 
 
 @blueprint.route('/get-layout')
 def get_layout():
-    return send_file("visual//static//images//layout.jpg", mimetype="image/jpg")
+    try:
+        return send_file("visual//static//images//layout.jpg", mimetype="image/jpg")
+    except Exception as e:
+        current_app.logger.exception("Exception occured", exc_info=True)
+        return
 
 
 @blueprint.route('/get-points')
 def get_points():
-    point_map = {'areas': [{
-        "name": "1",
-        "shape": "poly",
-        "coords": [25, 33, 27, 300, 128, 240, 128, 94],
-        "preFillColor": "transparent",
-        "fillColor": "#0000ff"
-    },
-        {
-        "name": "2",
-        "shape": "poly",
-        "coords": [219, 118, 220, 210, 283, 210, 284, 119],
-        "preFillColor": "transparent",
-        "lineWidth": 5,
-        "strokeColor": "#transparent"
-    },
-        {
-        "name": "3",
-        "shape": "poly",
-        "coords": [381, 241, 383, 94, 462, 53, 457, 282],
-        "preFillColor": "transparent",
-        "lineWidth": 5,
-        "strokeColor": "#transparent"
-    },
-        {
-        "name": "4",
-        "shape": "poly",
-        "coords": [245, 285, 290, 285, 274, 239, 249, 238],
-        "preFillColor": "transparent"
-    },
-        {
-        "name": "5",
-        "shape": "circle",
-        "coords": [170, 100, 10],
-        "preFillColor": "rgb(255,255,255,0.3)",
-        "lineWidth": 2
-    },
-        {
-        "name": "5.5",
-        "shape": "poly",
-        "coords": [170, 100, 170, 180, 246, 156],
-        "preFillColor": "transparent",
-        "fillColor": "#0000ff",
-        "strokeColor": "transparent"
-    },
+    try:
+        point_map = {'areas': [{
+            "name": "1",
+            "shape": "poly",
+            "coords": [25, 33, 27, 300, 128, 240, 128, 94],
+            "preFillColor": "transparent",
+            "fillColor": "#0000ff"
+        },
+            {
+            "name": "2",
+            "shape": "poly",
+            "coords": [219, 118, 220, 210, 283, 210, 284, 119],
+            "preFillColor": "transparent",
+            "lineWidth": 5,
+            "strokeColor": "#transparent"
+        },
+            {
+            "name": "3",
+            "shape": "poly",
+            "coords": [381, 241, 383, 94, 462, 53, 457, 282],
+            "preFillColor": "transparent",
+            "lineWidth": 5,
+            "strokeColor": "#transparent"
+        },
+            {
+            "name": "4",
+            "shape": "poly",
+            "coords": [245, 285, 290, 285, 274, 239, 249, 238],
+            "preFillColor": "transparent"
+        },
+            {
+            "name": "5",
+            "shape": "circle",
+            "coords": [170, 100, 10],
+            "preFillColor": "rgb(255,255,255,0.3)",
+            "lineWidth": 2
+        },
+            {
+            "name": "5.5",
+            "shape": "poly",
+            "coords": [170, 100, 170, 180, 246, 156],
+            "preFillColor": "transparent",
+            "fillColor": "#0000ff",
+            "strokeColor": "transparent"
+        },
 
-        {
-        "name": "6",
-        "shape": "rect",
-        "coords": [270, 100, 200, 50],
-        "lineWidth": 2,
-        "preFillColor": "rgba(255, 255, 255, 0.3)",
-        "strokeColor": "transparent"
-    }
-    ]}
-    return point_map
+            {
+            "name": "6",
+            "shape": "rect",
+            "coords": [270, 100, 200, 50],
+            "lineWidth": 2,
+            "preFillColor": "rgba(255, 255, 255, 0.3)",
+            "strokeColor": "transparent"
+        }
+        ]}
+        return point_map
+    except Exception as e:
+        point_map = {'areas': []}
+        current_app.logger.exception("Exception occured in sending the co-ordinates", exc_info=True)
+        return point_map
 
 
 @ blueprint.route('/get-status')
 def get_status():
-    rows = {"data": [{"name": "Kitchen PI - 04", "location": "Kitchen", "ipAddress": "192.168.0.13", "status": "connected"},
-                     {"name": "Kitchen PI - 01", 'location': 'Kitchen', 'ipAddress': '192.168.0.7', 'status': 'disconnected'},
-                     {"name": "Kitchen PI - 02", 'location': 'Kitchen', 'ipAddress': '192.168.0.19', 'status': 'disconnected'},
-                     {"name": "Kitchen PI - 03", 'location': 'Kitchen', 'ipAddress': '192.168.0.29', 'status': 'booting'}]}
-    return rows
+    try:
+        rows = {"data": [{"name": "Kitchen PI - 04", "location": "Kitchen", "ipAddress": "192.168.0.13", "status": "connected"},
+                         {"name": "Kitchen PI - 01", 'location': 'Kitchen', 'ipAddress': '192.168.0.7', 'status': 'disconnected'},
+                         {"name": "Kitchen PI - 02", 'location': 'Kitchen', 'ipAddress': '192.168.0.19', 'status': 'disconnected'},
+                         {"name": "Kitchen PI - 03", 'location': 'Kitchen', 'ipAddress': '192.168.0.29', 'status': 'booting'}]}
+        return rows
+    except Exception as e:
+        current_app.logger.exception("Exception occued in sending the data rows", exc_info=True)
+        rows = {"data": []}
+        return rows
