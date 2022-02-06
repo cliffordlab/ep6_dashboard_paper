5aca2d69cfe37cf8301755698ef73d0ec31648da
Ratansingh648 <ratansingh648@gmail.com>
2022-02-06 14:20:26 -0500

    Email Service to reset the password

---
+++ backend/app/authentication/email.py
@@ -0,0 +1,20 @@
+from flask_mailman import EmailMessage
+from flask import current_app
+
+
+def send_email(subject, body, send_to):
+    try:
+        msg = EmailMessage(
+            subject,
+            body,
+            current_app.config["EMAIL_SENDER"],
+            [send_to]
+        )
+        msg.content_subtype = "html"
+        msg.send()
+        return True
+    except:
+        current_app.logger.info('ERR - Cannot send email, please check MAIL settings')
+        current_app.logger.info('    -> MAIL Subject: ' + subject)
+        current_app.logger.info('    -> MAIL Body: ' + body)
+        return False
