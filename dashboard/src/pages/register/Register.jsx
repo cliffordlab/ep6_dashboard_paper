0306846eb7299f8f31e51311222f6f934393cf4c
Ratansingh648 <ratansingh648@gmail.com>
2022-11-06 02:36:21 -0500

    Correcting Register page with correct API calls

--- dashboard/src/pages/register/Register.jsx
+++ dashboard/src/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -10,13 +10,38 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import { config } from '../../environment';
+import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
 
-  const handleSubmit = (event) => {
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+
+
+  const HandleSubmit = (event) => {
       event.preventDefault();
-      console.log(event)
-  }
+
+      fetch(config.url.API_HOST + '/register', 
+      {   method : "POST", 
+          headers : { "Content-Type" : "application/json", },
+          body : JSON.stringify({"username" : firstName + " " + lastName  ,"email" : email, "password" : password})
+      })
+      .then(res => res.json())
+      .then(data => {
+          if(data.success){
+              alert(data.msg);
+              navigate("/login");
+          }
+          else{
+              alert(data.msg);
+          }
+      })
+      .catch((error) => { console.log(error);});
+      }
 
   return (
     <div>
@@ -26,23 +51,23 @@ export default function Register() {
           <Typography component="h1" variant="h5"> Sign up </Typography>
 
 
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box component="form" noValidate onSubmit={HandleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
               
               <Grid item xs={12} sm={6}>
-                <TextField autoComplete="given-name" name="firstName" required fullWidth id="firstName" label="First Name" autoFocus />
+                <TextField autoComplete="given-name" name="firstName" required fullWidth id="firstName" label="First Name" onChange={e => setFirstName(e.target.value)} autoFocus />
               </Grid>
 
               <Grid item xs={12} sm={6}>
-                <TextField required fullWidth id="lastName" label="Last Name" name="lastName" autoComplete="family-name" />
+                <TextField required fullWidth id="lastName" label="Last Name" name="lastName" onChange={e => setLastName(e.target.value)} autoComplete="family-name" />
               </Grid>
               
               <Grid item xs={12}>
-                <TextField required fullWidth id="email" label="Email Address" name="email" autoComplete="email" />
+                <TextField required fullWidth id="email" label="Email Address" name="email" onChange={e => setEmail(e.target.value)} autoComplete="email" />
               </Grid>
 
               <Grid item xs={12}>
-                <TextField required fullWidth name="password" label="Password" type="password" id="password" autoComplete="new-password" />
+                <TextField required fullWidth name="password" label="Password" type="password" id="password" onChange={e => setPassword(e.target.value)} autoComplete="new-password" />
               </Grid>
 
             </Grid>
