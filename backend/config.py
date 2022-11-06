883dde2c7275087b45c657cd0357feb335088860
Ratansingh648 <ratansingh648@gmail.com>
2022-11-06 02:33:40 -0500

    Replacing SQLite with MYSQL

--- backend/config.py
+++ backend/config.py
@@ -56,6 +56,11 @@ class ProductionConfig(Config):
     REMEMBER_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_DURATION = 3600
 
+    # Production uses MYSQL DB
+    DB_USERNAME = config("DB_USERNAME", "root")
+    DB_PASSWORD = config("DB_PASSWORD", "rs@648")
+    SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@localhost/ep6'.format(DB_USERNAME, DB_PASSWORD)
+
 
 # UAT Specific configurations go here
 class UATConfig(Config):
@@ -67,6 +72,10 @@ class UATConfig(Config):
 class DevConfig(Config):
     DEBUG = True
     ENV = "DEV"
+
+    DB_USERNAME = config("DB_USERNAME", "root")
+    DB_PASSWORD = config("DB_PASSWORD", "rs648")
+    SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@localhost/ep6'.format(DB_USERNAME, DB_PASSWORD)
 
 
 # Load all possible configurations
